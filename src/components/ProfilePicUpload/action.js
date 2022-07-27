e9edc3af501121655a7e41764ccfa498753a5f21
Vineet2408 <46402936+Vineet2408@users.noreply.github.com>
2022-07-27 11:54:59 +0530

    Add files via upload

--- src/components/ProfilePicUpload/action.js
+++ src/components/ProfilePicUpload/action.js
@@ -1,18 +1,53 @@
 
 import axios from "axios";
-import { PHOTO_UPLOAD_API } from "../../constants/api";
+import { PHOTO_UPLOAD_API, PROFILE_PIC_NAME_API,  DOWNLOAD_PROFILE_PIC_API} from "../../constants/api";
 
 export const PHOTO_UPLOAD = 'PHOTO_UPLOAD';
-export const uploadPhoto = (userId, photoData) => function (dispatch) {
+export const UPDATE_USERNAME_AND_PROFILEPIC_NAME = "UPDATE_USERNAME";
+export const DOWNLOAD_PROFILE_PIC = "DOWNLOAD_PROFILE_PIC"; 
+export const uploadPhoto = (username, imageData, setters) =>async  function (dispatch) {
 
-    const axiosConfig= {method: 'put', url: PHOTO_UPLOAD_API};
+    let response;
+        try {
+            response= await axios.post(`http://localhost:8083/file/update/profilepic/${username}`,
+            imageData,{
+                onUploadProgress: progressEvent=>{
+                    setTimeout(() => {
+                        setters.setProgressWidth((progressEvent.loaded *100/progressEvent.total).toString()+"%");
+                    }, 1000);
+                }
+            }
+            );
+            console.log('response = ', response);
+            dispatch({
+                type:PHOTO_UPLOAD,
+                payload: {avatarLink:DOWNLOAD_PROFILE_PIC_API+response.data.avatarLink}
+            });
+                setters.setButtons(false);
+                setTimeout(() => {
+                    
+                    setters.setProgressWidth("0px");
+                }, 2000);
+            
+        }
+        catch(e) {
+            console.log(e);
+        }
+       
+   
+}
 
-    axios(axiosConfig)
-    .then((response) => {
-        dispatch({
-            type:PHOTO_UPLOAD,
-            payload: response.data
-        })
+export const getProfilePicName = (username)=> function (dispatch) {
+    const axiosConfig = {method: 'get', url: PROFILE_PIC_NAME_API+username};
+    axios(axiosConfig).then((response) => {
+        dispatch(
+            {
+                type: UPDATE_USERNAME_AND_PROFILEPIC_NAME, 
+                payload:{
+                    username,
+                    profilePicName: response.data
+                }
+            });
     })
-    .catch((error) => console.log(error));
+    .catch((e)=>console.log(e));
 }
